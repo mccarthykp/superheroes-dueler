7fd8408e49baa12c838180f80d3b36da25a08a82
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-11-05 14:36:43 -0400

    added team test file and updated team class with all methods except attack

--- team.py
+++ team.py
@@ -3,7 +3,7 @@ from hero import Hero
 class Team():
     def __init__(self, name):
         self.name = name
-        self.heroes = list()
+        self.heroes = []
 
     def add_hero(self, hero):
         ''' Add hero object to self.heroes. '''
@@ -27,3 +27,34 @@ class Team():
         # TODO: Loop over list of heroes and print their names.
         for hero in self.heroes:
             print(hero.name)
+
+    def stats(self):
+        ''' Print team statistics '''
+        for hero in self.heroes:
+            kd = hero.kills / hero.deaths
+            print(f'{hero.name} Kill/Deaths: {kd}')
+
+    def revive_heroes(self):
+        ''' Reset all heroes health to starting health. '''
+        # TODO: for each hero in self.heroes, set the hero's current_health to their starting_health.
+        for hero in self.heroes:
+            hero.current_hp = hero.starting_hp
+
+    def attack(self, other_team):
+        ''' Battle each team against each other. '''
+
+        living_heroes = []
+        living_opponents = []
+
+        for hero in self.heroes:
+            living_heroes.append(hero)
+
+        for hero in other_team.heroes:
+            living_opponents.append(hero)
+
+        while len(living_heroes) > 0 and len(living_opponents) > 0:
+            # TODO: Complete the following:
+            # 1. Randomly select living hero from each team (random.choice)
+            # 2. Have the heroes fight each other (use fight method in Hero class)
+            # 3. update the list of living_heroes and living_opponents to reflect the result of the fight
+            pass
